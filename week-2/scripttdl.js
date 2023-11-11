309956827e322ff7a639c3847c402ad7262b08f4
Rahulmahanta <134363265+Rahulmahanta@users.noreply.github.com>
2023-11-11 21:18:32 +0530

    Add files via upload

---
+++ week-2/scripttdl.js
@@ -0,0 +1,25 @@
+const inputBox = document.getElementById("input-box")
+const taskContainer= document.getElementById("task-container")
+function addTask(){
+    if(inputBox.value ===''){
+        alert("Enter Task!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        taskContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+taskContainer.addEventListener("click" , function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
